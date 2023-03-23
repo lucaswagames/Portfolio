9da546d2f600a8be74caefabb2f191cab8801616
lucaswagames <lucas.charbonnier43@gmail.com>
2023-03-23 13:55:44 +0100

    pdf bug corrected

--- pages/index.tsx
+++ pages/index.tsx
@@ -11,9 +11,10 @@ import { useEffect, useState } from 'react';
 
 export default function About() {
   const [height, setHeight] = useState(0);
-
+  const [width, setWidth] = useState(0);
   useEffect(() => {
     setHeight(window.innerHeight);
+    setWidth(window.innerWidth);
   }, []);
 
   return (
@@ -54,7 +55,7 @@ export default function About() {
             <object
               data="/CV.pdf"
               type="application/pdf"
-              width="1000"
+              width={width/2}
               height={height}
             >
               <p>
