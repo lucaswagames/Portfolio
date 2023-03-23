600145fbbd3251495c5b47a2aeee825408a9080b
lucaswagames <lucas.charbonnier43@gmail.com>
2023-03-23 13:33:39 +0100

    update skill and project component

--- pages/components/ProjectCard.tsx
+++ pages/components/ProjectCard.tsx
@@ -1,35 +1,57 @@
-import Image from 'next/image';
 import styles from '../../styles/layout.module.css';
 
 interface ProjectCardProps {
-    video: string;
-    title: string;
-    content: {
-      goal?: string;
-      description?: string;
-      techno?: string;
-      projectType?: string;
-    };
-  }
-  
-  export default function ProjectCard({ video, title, content }: ProjectCardProps) {
-      const goal = content.goal ?? '';
-      const description = content.description ?? '';
-      const techno = content.techno ?? '';
-      const projectType = content.projectType ?? '';
-      return (
-          <>
-              <video src={video} controls width="500">
-                  Your browser does not support the video tag.
-              </video>
-              <div className={styles.projectTexte}>
-                  <h2>{title}</h2>
-                  <p> <b><u>But</u> : </b> {goal}</p>
-                  <p> <b><u>Description</u> : </b>{description}</p>
-                  <p> <b><u>Technologies</u> : </b>{techno}</p>
-                  <p><b><u>Type de projet</u> : </b>{projectType}</p>
-              </div>
-          </>
-      );
-  }
-  
+  video: string;
+  title: string;
+  content: {
+    goal?: string;
+    description?: string;
+    techno?: string;
+    projectType?: string;
+  };
+}
+
+export default function ProjectCard({ video, title, content }: ProjectCardProps) {
+  const goal = content.goal ?? '';
+  const description = content.description ?? '';
+  const techno = content.techno ?? '';
+  const projectType = content.projectType ?? '';
+
+  return (
+    <>
+      <video src={video} controls width="500">
+        Your browser does not support the video tag.
+      </video>
+      <div className={styles.projectTexte}>
+        <h2>{title}</h2>
+        <p>
+          {' '}
+          <b>
+            <u>But</u> :
+          </b>{' '}
+          {goal}
+        </p>
+        <p>
+          {' '}
+          <b>
+            <u>Description</u> :
+          </b>
+          {description}
+        </p>
+        <p>
+          {' '}
+          <b>
+            <u>Technologies</u> :
+          </b>
+          {techno}
+        </p>
+        <p>
+          <b>
+            <u>Type de projet</u> :
+          </b>
+          {projectType}
+        </p>
+      </div>
+    </>
+  );
+}
--- pages/components/SkillCard.tsx
+++ pages/components/SkillCard.tsx
@@ -2,19 +2,19 @@ import Image from 'next/image';
 import styles from '../../styles/layout.module.css';
 
 interface SkillCardProps {
-    img: string;
-    content: {
-        name: string;
-    };
+  img: string;
+  content: {
+    name: string;
+  };
 }
 
 export default function SkillCard({ img, content }: SkillCardProps) {
-    return (
-        <>
-            <div className={styles.skillSolo}>
-                <Image src={img} alt="Picture of the skill" width={60} height={60} />
-                <h2>{content.name}</h2>
-            </div>
-        </>
-    );
+  return (
+    <>
+      <div className={styles.skillSolo}>
+        <Image src={img} alt="Picture of the skill" width={60} height={60} />
+        <h2>{content.name}</h2>
+      </div>
+    </>
+  );
 }
